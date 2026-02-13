dfcd92d0532910564d888c215b19609c23c30f48
Sinisa <sinisa.druzeta@gmail.com>
2026-02-13 11:26:00 +0100

    migrated full performance tests from old indago

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-INDAGO OPTIMIZERS TEST
-a (mostly) comprehensive test of Indago optimizers
+INDAGO OPTIMIZERS PERFORMANCE TEST
+a (mostly) comprehensive test of Indago optimizers performance
 
-SHOULD BE RUN AT EVERY INDAGO UPGRADE
-A TEST FOR EVERY NEW OPTIMIZER PERFORMANCE-RELATED FEATURE SHOULD BE ADDED HERE
+A TEST FOR EVERY NEW METHOD/FEATURE PERFORMANCE SHOULD BE ADDED HERE
 """
 
 # need this for local (non-pip) install only
@@ -40,6 +39,16 @@ def test_PSO_defaults() -> None:
     description = 'PSO defaults'
     optimizer = PSO()
     expected_result = 2.5793297920299136
+    tolerance = TOL
+    result = run(optimizer)
+    assert expected_result - tolerance < result < expected_result + tolerance, \
+        f'{description} FAILED, result={result}, expected={expected_result}'
+
+def test_PSO_defaults_solution_given():
+    description = 'PSO defaults, solution given (1D X0)'
+    optimizer = PSO()
+    optimizer.X0 = np.zeros(DIM)
+    expected_result = 0.0
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
@@ -231,207 +240,197 @@ def test_PSO_defaults_halton_initializer() -> None:
         f'{description} FAILED, result={result}, expected={expected_result}'
 
 """
-def test_FWA_defaults() -> None:
+def test_FWA_defaults():
     description = 'FWA defaults'
     optimizer = FWA()
-    expected_result = 3.200343710545890
+    expected_result = -14.3552157847343
     tolerance = 1e-4
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_FWA_defaults_1D_X0() -> None:
-    description = 'FWA defaults with 1D X0'
-    optimizer = FWA()
-    optimizer.X0 = np.zeros(DIM)
-    expected_result = 4.0215439156384525
-    tolerance = TOL
-    result = run(optimizer)
-    assert expected_result - tolerance < result < expected_result + tolerance, \
-        f'{description} FAILED, result={result}, expected={expected_result}'
-
-def test_FWA_custom_parameters() -> None:
+def test_FWA_custom_parameters():
     description = 'FWA custom parameters'
     optimizer = FWA()
     optimizer.params['n'] = 12
     optimizer.params['m1'] = 8
     optimizer.params['m2'] = 6
-    expected_result = 3.7916074172176613
+    expected_result = -12.384148004226958
     tolerance = 1e-4
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_SSA_defaults() -> None:
+def test_SSA_defaults():
     description = 'SSA defaults'
     optimizer = SSA()
-    expected_result = 4.557141362961143
+    expected_result = 4.850766721900909
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_SSA_custom_parameters() -> None:
+def test_SSA_custom_parameters():
     description = 'SSA custom parameters'
     optimizer = SSA()
     optimizer.params['pop_size'] = 12
     optimizer.params['ata'] = 0.8
-    expected_result = 4.217644352979585
+    expected_result = 4.438743024215149
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_SSA_custom_additional_parameters() -> None:
+def test_SSA_custom_additional_parameters():
     description = 'SSA custom additional parameters'
     optimizer = SSA()
     optimizer.params['pop_size'] = 12
     optimizer.params['p_pred'] = 0.2
     optimizer.params['c_glide'] = 1.5
-    expected_result = 4.852974034102223
+    expected_result = 4.47154424234304
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_DE_defaults() -> None:
+def test_DE_defaults():
     description = 'DE defaults'
     optimizer = DE()
-    expected_result = 4.06562470951338
+    expected_result = 2.3996066775068625
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_DE_LSHADE_defaults() -> None:
+def test_DE_LSHADE_defaults():
     description = 'DE SHADE defaults'
     optimizer = DE()
     optimizer.variant = 'SHADE'
-    expected_result = 4.059037126643917
+    expected_result = 3.3448851150990944
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_DE_LSHADE_custom_parameters() -> None:
+def test_DE_LSHADE_custom_parameters():
     description = 'DE LSHADE custom parameters'
     optimizer = DE()
     optimizer.variant = 'LSHADE'
     optimizer.params['pop_init'] = 20
     optimizer.params['f_archive'] = 2
-    expected_result = 3.927170854340928
+    expected_result = 1.4083783202220084
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_BA_defaults() -> None:
+def test_BA_defaults():
     description = 'BA defaults'
     optimizer = BA()
-    expected_result = 5.016784239297477
+    expected_result = 4.213229947486668
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_EFO_defaults() -> None:
+def test_EFO_defaults():
     description = 'EFO defaults'
     optimizer = EFO()
-    expected_result = 4.345023660392395
+    expected_result = 1.01888834670826
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_MRFO_defaults() -> None:
+def test_MRFO_defaults():
     description = 'MRFO defaults'
     optimizer = MRFO()
-    expected_result = 5.028612889414191
+    expected_result = 3.265045494474331
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_ABC_defaults() -> None:
+def test_ABC_defaults():
     description = 'ABC defaults'
     optimizer = ABC()
-    expected_result = 4.678938084932741
+    expected_result = 4.33976322802834
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_ABC_FullyEmployed_defaults() -> None:
+def test_ABC_FullyEmployed_defaults():
     description = 'ABC FullyEmployed defaults'
     optimizer = ABC()
     optimizer.variant = 'FullyEmployed'
-    expected_result = 4.945925069777881
+    expected_result = 3.716602431815081
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_ABC_Vanilla_custom_parameters() -> None:
+def test_ABC_Vanilla_custom_parameters():
     description = 'ABC Vanilla custom parameters'
     optimizer = ABC()
     optimizer.variant = 'Vanilla'
     optimizer.params['pop_size'] = 20
     optimizer.params['trial_limit'] = 50
-    expected_result = 4.678938084932741
+    expected_result = 4.33976322802834
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_GWO_defaults() -> None:
+def test_GWO_defaults():
     description = 'GWO defaults'
     optimizer = GWO()
-    expected_result = 4.158491127774654
+    expected_result = -3.6171180468126125
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_GWO_HSA_defaults() -> None:
+def test_GWO_HSA_defaults():
     description = 'GWO HSA defaults'
     optimizer = GWO()
     optimizer.variant = 'HSA'
-    expected_result = 4.218103907364332
+    expected_result = -7.0488083215685196
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_MSGD_defaults() -> None:
+def test_MSGD_defaults():
     description = 'MSGD defaults'
     optimizer = MSGD()
-    expected_result = 3.9757473965378893
+    expected_result = 0  # TODO: -inf in old Indago
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_NM_defaults() -> None:
+def test_NM_defaults():
     description = 'NM defaults'
     optimizer = NM()
-    expected_result = 4.13236103332557
+    expected_result = 4.197968360852144
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_NM_Vanilla_defaults() -> None:
+def test_NM_Vanilla_defaults():
     description = 'NelderMead Vanilla defaults'
     optimizer = NM()
     optimizer.variant = 'Vanilla'
-    expected_result = 4.1220197428515535
+    expected_result = 4.099420846773123
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 """
 
-def test_RS_defaults() -> None:
+def test_RS_defaults():
     description = 'RS defaults'
     optimizer = RS()
     expected_result = 4.649421244751145
@@ -440,7 +439,7 @@ def test_RS_defaults() -> None:
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_RS_halton_initializer() -> None:
+def test_RS_halton_initializer():
     description = 'RS halton initializer'
     optimizer = RS()
     optimizer.sampler = 'halton'
@@ -450,7 +449,7 @@ def test_RS_halton_initializer() -> None:
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_RS_sobol_initializer() -> None:
+def test_RS_sobol_initializer():
     description = 'RS sobol initializer'
     optimizer = RS()
     optimizer.sampler = 'sobol'
@@ -460,7 +459,7 @@ def test_RS_sobol_initializer() -> None:
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_RS_lhs_initializer() -> None:
+def test_RS_lhs_initializer():
     description = 'RS lhs initializer'
     optimizer = RS()
     optimizer.sampler = 'lhs'
@@ -471,69 +470,69 @@ def test_RS_lhs_initializer() -> None:
         f'{description} FAILED, result={result}, expected={expected_result}'
 
 """
-def test_HBO_defaults() -> None:
+def test_HBO_defaults():
     description = 'HBO defaults'
     optimizer = HBO()
-    expected_result = 4.2530537129953
+    expected_result = 4.166997012436769
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_HBO_Dynamic_defaults() -> None:
+def test_HBO_Dynamic_defaults():
     description = 'HBO Dynamic defaults'
     optimizer = HBO()
     optimizer.variant = 'Dynamic'
-    expected_result = 4.555548715599656
+    expected_result = 4.72960644554505
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_CRS_defaults() -> None:
+def test_CRS_defaults():
     description = 'CRS defaults'
     optimizer = CRS()
-    expected_result = 4.407850741909356
+    expected_result = 4.437560183138608
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_CRS_custom_parameters_1() -> None:
+def test_CRS_custom_parameters_1():
     description = 'CRS custom parameters 1'
     optimizer = CRS()
     optimizer.params['pop_scale'] = 1
-    expected_result = 5.119988901716537
+    expected_result = 4.525681028714209
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_CRS_custom_parameters_2() -> None:
+def test_CRS_custom_parameters_2():
     description = 'CRS custom parameters 2'
     optimizer = CRS()
     optimizer.params['pop_scale'] = 3.3
-    expected_result = 4.609663346899482
+    expected_result = 4.778298365107319
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_EEEO_defaults() -> None:
+def test_EEEO_defaults():
     description = 'EEEO defaults'
     optimizer = EEEO()
-    expected_result = 4.468478265803395
+    expected_result = -2.825058356190423
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
         f'{description} FAILED, result={result}, expected={expected_result}'
 
-def test_EEEO_custom_parameters() -> None:
+def test_EEEO_custom_parameters():
     description = 'EEEO custom parameters'
     optimizer = EEEO()
     optimizer.methods = {'DE': ('LSHADE', {'pop_init': 30}),
                          'GWO': ('Vanilla', {'pop_size': 20})}
-    expected_result = 4.235634551617493
+    expected_result = 1.6084339646218162
     tolerance = TOL
     result = run(optimizer)
     assert expected_result - tolerance < result < expected_result + tolerance, \
@@ -548,6 +547,7 @@ if __name__ == '__main__':
     print('Running tests ...')
 
     test_PSO_defaults()
+    test_PSO_defaults_solution_given()
     test_PSO_defaults_1D_X0()
     test_PSO_defaults_2D_X0()
     test_PSO_defaults_int_X0()
